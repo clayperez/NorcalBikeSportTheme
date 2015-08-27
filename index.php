52a4dce67c5863e85bd0c60cf1ff93c84c12f26f
clayperez <clayperez@gmail.com>
2015-08-26 22:34:19 -0700

    Update

--- index.php
+++ index.php
@@ -1,39 +1,39 @@
 <?php get_header(); ?>
 
-  <div class="animated fadeInUp">
+<?php if(is_front_page()) { ?>
+  <div class="animated fadeInUp">    
+    <div class="row">
+      <div class="large-7 columns pinupAltFloater">
+        <?php the_field('pinup1'); ?>
+      </div>
+      <div class="large-5 columns pinupAltFloater">
+        <?php the_field('pinup2'); ?>
+      </div>
+    </div>
     
     <div class="row">
-    <div class="large-7 columns pinup1">
-      <?php the_field('pinup1'); ?>
+      <div class="large-5 columns pinupAltFloater">
+        <?php the_field('pinup3'); ?>
+      </div>
+      <div class="large-7 columns youtubeWrapper">
+        <iframe src="<?php the_field('youtube1'); ?>" frameborder="0" allowfullscreen></iframe>
+      </div>
     </div>
-    <div class="large-5 columns">
-      <img src="<?php the_field('pinup2'); ?>">
-    </div>
-  </div>
-    
-    <div class="row">
-    <div class="large-5 columns">
-      <a href="shoprides.htm"><img src="<?php the_field('pinup3'); ?>"></a>
-    </div>
-    <div class="large-7 columns">
-      <iframe width="620" height="349" src="<?php the_field('youtube1'); ?>" frameborder="0" allowfullscreen></iframe>
-    </div>
-  </div>
     
     
     <div class="row">
-    <div class="large-4 columns">
-      <img src="<?php the_field('pinup4'); ?>">
-    </div>
-    <div class="large-4 columns">
-      <img src="<?php the_field('pinup5'); ?>">
-    </div>
-    <div class="large-4 columns">
-      <a href="shoprides.htm"><img src="<?php the_field('pinup6'); ?>"></a>
-    </div>
+      <div class="large-4 columns pinupAltFloater">
+        <?php the_field('pinup4'); ?>
+      </div>
+      <div class="large-4 columns pinupAltFloater">
+        <?php the_field('pinup5'); ?>
+      </div>
+      <div class="large-4 columns pinupAltFloater">
+        <?php the_field('pinup6'); ?>
+      </div>
+    </div>  
   </div>
-  
-  </div>
+<?php } ?>
 
   <?php
     if ( have_posts() ) {
--- js/script.js
+++ js/script.js
@@ -5,8 +5,9 @@ jQuery(document).ready(function($){
   /////////////////////////////////
   // PINUP IMAGE TITLE RENDERING //
   /////////////////////////////////
-  var $imageObject = $(".pinup1 img");
-  var imageTitle = $imageObject.attr("alt");
-  $imageObject.before("<div class='imageTitleFloater'>" + imageTitle + "</div>");
+  $(".pinupAltFloater img").each(function(){
+    var $dom = $(this);
+    $dom.before("<div class='imageTitleFloater'>" + $dom.attr("alt") + "</div>");
+  });
 
 });
