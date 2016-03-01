e5e8706740941a78a0806edd9c3f74e0de3bb0d4
Carlos Perez <clayperez@gmail.com>
2016-03-01 14:20:41 -0800

    Fixes to get rid of some errors

--- footer.php
+++ footer.php
@@ -38,8 +38,20 @@
   <script src="<?php bloginfo('template_url'); ?>/js/foundation.min.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/js/script.js"></script>
   
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-744482-7', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
+
   <script>
-    $(document).foundation();
+    jQuery(document).foundation();
     window.onload = function() {
         setTimeout(function(){window.scrollTo(0, 1);}, 1);
     }
